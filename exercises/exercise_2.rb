d6e4149e43de68df072012c599021faaf5bd3a27
Tammy <tammyanndo@gmail.com>
2021-11-11 12:45:28 -0500

    completed exercise 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+puts @store1
+puts @store2
+
+@store1.name = "Toronto"
+
+@store1.save
+
+puts @store1.inspect
+puts @store2.inspect
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,27 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts @mens_stores.inspect
+
+@mens_stores.each do |store|
+print store.name 
+print " "
+p store.annual_revenue
+end
+
+@womens_stores = Store.where("womens_apparel = ? and annual_revenue < ?", true, 1000000)
+
+puts @womens_stores.inspect
+
+@womens_stores.each do |store|
+  print store.name
+  print " "
+  p store.annual_revenue
+end 
