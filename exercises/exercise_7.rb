6fe0fa4667ac2d947f8077d4d9711a4ef65c9468
Tammy <tammyanndo@gmail.com>
2021-11-11 14:29:35 -0500

    completed exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter in the name of a store:"
+@user_store = gets.chomp
+
+user_store = Store.create(name: @user_store)
+puts Store.count
+
+# puts user_store.errors.full_messages.each
+
+user_store.errors.full_messages.each do |error|
+puts error
+end
+
+
+
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true
+  validates :annual_revenue, numericality: { greater_than: 0 }
 end
