d8c5b057eae006831249d372c7e18448c1a1be44
Tammy <tammyanndo@gmail.com>
2021-11-11 12:54:50 -0500

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_annual_revenue = Store.sum(:annual_revenue)
+puts @total_annual_revenue
+puts "Total store revenue: #{@total_annual_revenue}"
+
+@total_number_of_stores = Store.count
+puts @total_number_of_stores
+puts "Total number of stores: #{@total_number_of_stores}"
+
+@average_annual_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue: #{@average_annual_revenue}"
+
