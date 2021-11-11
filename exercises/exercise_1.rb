b2e18dbffc5f18487dffb8f8fa695d4912448c54
Tammy <tammyanndo@gmail.com>
2021-11-11 10:20:10 -0500

    completed exericise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -10,3 +10,18 @@ Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, wom
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+puts @store1
+puts @store2
+@store1.name = "Toronto"
+@store1.save
+puts @store1
+
+puts "Store count BEFORE destroy: #{Store.count}"
+
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts "Store count AFTER destroy: #{Store.count}"
